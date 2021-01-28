6deacffaefddbac4746a9019fd5198eaa64d0bbe
ellenupark <ellenupark@gmail.com>
2021-01-28 00:34:47 -0500

    heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: "_backend", domain: "http://teachers-pet-online-backend.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_backend", domain: "http://teachers-pet-online.herokuapp.com/"
     # Rails.application.config.session_store :cookie_store, key: '_backend'
 else
     Rails.application.config.session_store :cookie_store, key: '_backend' 
