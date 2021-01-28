9357925dd185ac5b37902f3b76341d1b44566de1
ellenupark <ellenupark@gmail.com>
2021-01-28 01:11:01 -0500

    Fixed url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'http://teachers-pet-online.herokuapp.com/'
+      origins 'http://teachers-pet-online.herokuapp.com'
     
       resource '*',
         headers: :any,
